6e90d80d383e7a5d62ab18eb8f613112a7635f65
JisooOh94 <chocokon@gmail.com>
2021-02-14 03:40:44 +0900

    [#1] Property 주입 활성화

--- src/main/java/controller/TestController.java
+++ src/main/java/controller/TestController.java
@@ -1,16 +1,27 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author jisoooh
  */
+@Controller
 @RequestMapping("/test")
 public class TestController {
+	@Value("${env}")
+	private String env;
 
 	@GetMapping("/helloWorld")
 	public String helloWorld() {
+		return "index";
+	}
+
+	@GetMapping("/property/injection")
+	public String propertyInjection() {
+		System.out.println(env);
 		return "index";
 	}
 }
