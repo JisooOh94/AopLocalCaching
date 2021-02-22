772e0f6671029f569230a419822f3d2c784c5f99
JisooOh94 <chocokon@gmail.com>
2021-02-23 03:16:00 +0900

    [#1] property key > static final 변수로 수정

--- src/main/java/config/ServletContextConfig.java
+++ src/main/java/config/ServletContextConfig.java
@@ -11,6 +11,9 @@ import controller.TestController;
 @Configuration
 @PropertySource("classpath:config.properties")
 public class ServletContextConfig {
+	private static final String PROPERTY_VIEW_PREFIX_KEY = "view.prefix";
+	private static final String PROPERTY_VIEW_SUFFIX_KEY = "view.suffix";
+	private static final String PROPERTY_ENV_KEY = "env";
 	private final Environment environment;
 
 	@Autowired
@@ -20,13 +23,13 @@ public class ServletContextConfig {
 
 	@Bean
 	public TestController testController() {
-		return new TestController(environment.getProperty("env"));
+		return new TestController(environment.getProperty(PROPERTY_ENV_KEY));
 	}
 
 	@Bean
 	public InternalResourceViewResolver viewResolver() {
-		String prefix = environment.getProperty("view.prefix");
-		String suffix = environment.getProperty("view.suffix");
+		String prefix = environment.getProperty(PROPERTY_VIEW_PREFIX_KEY);
+		String suffix = environment.getProperty(PROPERTY_VIEW_SUFFIX_KEY);
 		return new InternalResourceViewResolver(prefix, suffix);
 	}
 }
